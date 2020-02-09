ce3778e4881c2aca13f91add28929c5c7dd91aec
Mohammad Shoaei <mohammad.shoaei@outlook.com>
2020-02-10 00:33:19 +0330

    fixed issue caused by sending numbers as strings. auto scroll

--- src/components/StreamForm.vue
+++ src/components/StreamForm.vue
@@ -2,19 +2,39 @@
   <v-container id="wrapper" class="d-flex flex-column justify-space-between">
     <v-row>
       <v-col cols="12">
-        <v-textarea readonly :rows="rows" :value="streamData"></v-textarea>
+        <v-textarea
+          id="streamArea"
+          readonly
+          :rows="rows"
+          clearable
+          no-resize
+          :value="streamData"
+        ></v-textarea>
       </v-col>
     </v-row>
     <v-form class="d-flex" id="form">
       <v-row class="">
         <v-col cols="6" md="4">
-          <v-text-field label="File name" autofocus required></v-text-field>
+          <v-text-field
+            label="File name"
+            v-model.trim="form.file"
+            autofocus
+            required
+          ></v-text-field>
         </v-col>
         <v-col cols="6" md="4">
-          <v-text-field label="Delay (ms)" required></v-text-field>
+          <v-text-field
+            label="Delay (ms)"
+            v-model.number="form.skip"
+            required
+          ></v-text-field>
         </v-col>
         <v-col cols="6" md="4">
-          <v-text-field label="Duration (s)" required></v-text-field>
+          <v-text-field
+            label="Duration (ms)"
+            v-model.number="form.duration"
+            required
+          ></v-text-field>
         </v-col>
         <v-col cols="6" md="12">
           <v-btn
@@ -38,24 +58,32 @@ export default {
   name: "StreamForm",
   data() {
     return {
+      form: {} as {
+        file: string;
+        skip: number;
+        duration: number;
+      },
       streamData: "",
       count: 0
     };
   },
   methods: {
     stream: function(this: any) {
+      let streamArea = document.getElementById("streamArea") as HTMLElement;
       axios
-        .post("/api/command", JSON.stringify(this.finalData))
+        .post("/api/readlive", JSON.stringify(this.form))
         .then(() => {
           let conn = new WebSocket(
             "ws://" + window.location.host + "/api/readlive"
           );
           conn.onmessage = (event: MessageEvent) => {
             this.streamData += event.data + "\n";
+            streamArea.scrollTop = streamArea.scrollHeight;
           };
           conn.onclose = (event: CloseEvent) => {
             this.streamData += "connection closed\n";
-            this.streamData += event.code;
+            this.streamData += event.code + "\n";
+            streamArea.scrollTop = streamArea.scrollHeight;
             window.location.replace("/api/getfile");
           };
         })
